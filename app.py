95e0bd178dd13d2d2af0abcc672584bfed4aee47
Harkt192 <malysh9002@gmail.com>
2025-04-14 00:33:22 +0300

    First Commit

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def base_page():  # put application's code here
 def index_page():
     return "И на Марсе будут яблони цвести!"
 
+@app.route("/promotion")
+def promotion_page():
+    text = """Человечество вырастает из детства.</br>
+    Человечеству мала одна планета.</br>
+    Мы сделаем обитаемыми безжизненные пока планеты.</br>
+    И начнем с Марса! Присоединяйся!"""
+    return text
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080)

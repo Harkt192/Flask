3442a1dfdede113aa35ff5fc6c8a73726bd22972
Harkt192 <malysh9002@gmail.com>
2025-04-19 02:20:17 +0300

    Commit #2

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
-
+app = flask.Flask(__name__)
 
 @app.route('/')
-def base_page():  # put application's code here
-    return 'Миссия Колонизация Марса'
+def base_page():
+    return flask.render_template("base.html")
 
 @app.route("/index")
 def index_page():
-    return "И на Марсе будут яблони цвести!"
+    return flask.render_template("index.html")
 
 @app.route("/promotion")
 def promotion_page():
-    text = """Человечество вырастает из детства.</br>
-    Человечеству мала одна планета.</br>
-    Мы сделаем обитаемыми безжизненные пока планеты.</br>
-    И начнем с Марса! Присоединяйся!"""
-    return text
+    return flask.render_template("promotion.html")
 
 @app.route("/image_mars")
 def image_mars_page():
-    return r"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="static/images/mars.png" />
-                    <p>Вот она какая, красная планета.</p>
-                  </body>
-                </html>"""
+    return flask.render_template("image_mars.html")
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return flask.render_template("bootstrap_sample.html")
+
+@app.route("/promotion_image")
+def promotion_image_page():
+    return
 
 
 if __name__ == '__main__':

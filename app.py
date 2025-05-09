29c5b1ee94eef42fddcad1faea7cf9c058a21b1d
Harkt192 <malysh9002@gmail.com>
2025-05-09 18:46:25 +0300

    Загрузка файла

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
-import flask
+import os
+from os.path import exists
 
-app = flask.Flask(__name__)
+from flask import Flask, request, render_template, redirect
+import threading
+
+app = Flask(__name__)
 
 @app.route('/')
 def base_page():
-    return flask.render_template("main.html")
+    return render_template("main.html")
 
 @app.route("/index")
 def index_page():
-    return flask.render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/promotion")
 def promotion_page():
-    return flask.render_template("promotion.html")
+    return render_template("promotion.html")
 
 @app.route("/image_mars")
 def image_mars_page():
-    return flask.render_template("image_mars.html")
+    return render_template("image_mars.html")
 
 @app.route('/bootstrap_sample')
 def bootstrap():
-    return flask.render_template("bootstrap_sample.html")
+    return render_template("bootstrap_sample.html")
 
 @app.route("/promotion_image")
 def promotion_image_page():
-    return flask.render_template("promotion_image.html")
+    return render_template("promotion_image.html")
 
 @app.route('/astronaut_selection', methods=['POST', 'GET'])
 def form_sample():
-    if flask.request.method == 'GET':
-        return flask.render_template("astronaut_selection.html")
-    elif flask.request.method == 'POST':
-        print(flask.request.form['surname'])
-        print(flask.request.form['name'])
-        print(flask.request.form['email'])
-        print(flask.request.form['class'])
+    if request.method == 'GET':
+        return render_template("astronaut_selection.html")
+    elif request.method == 'POST':
+        print(request.form['surname'])
+        print(request.form['name'])
+        print(request.form['email'])
+        print(request.form['class'])
 
         jobs = []
         for job in [
@@ -48,20 +52,20 @@ def form_sample():
             "Exobiologist"
         ]:
             try:
-                if flask.request.form[job]:
+                if request.form[job]:
                     jobs.append(job)
             except Exception:
                 pass
         print(jobs)
-        print(flask.request.form['sex'])
-        print(flask.request.form['about'])
+        print(request.form['sex'])
+        print(request.form['about'])
 
-        if flask.request.form['file']:
-            print(flask.request.form['file'])
+        if request.form['file']:
+            print(request.form['file'])
         else:
             print("No file")
         try:
-            print(flask.request.form['accept'])
+            print(request.form['accept'])
         except Exception:
             print("No accept")
         return "<h1>Форма отправлена</h1>"
@@ -69,18 +73,65 @@ def form_sample():
 @app.route("/choice/<string:planet_name>")
 def choose_planet(planet_name):
     if planet_name == "Марс":
-        return flask.render_template("mars.html")
+        return render_template("mars.html")
     elif planet_name == "Юпитер":
-        return flask.render_template("jupiter.html")
+        return render_template("jupiter.html")
 
 @app.route("/results/<string:nickname>/<int:level>/<float:rating>")
 def results(nickname, level, rating):
-    return flask.render_template(
+    return render_template(
         "results.html",
         nickname=nickname,
         level=level,
         rating=rating
     )
+
+
+LAST_FILENAME = ""
+
+
+@app.route('/load_photo', methods=["GET", "POST"])
+def upload_file():
+    global LAST_FILENAME
+    filename = ""
+    if request.method == "POST":
+        if "file" not in request.files:
+            if "send_file" in request.form:
+                if not request.form["send_file"]:
+                    return render_template('upload.html', message="Выберите файл", filename=filename)
+
+        if "send_file" in request.form:
+            temp_filename = request.form["send_file"]
+            filename = temp_filename.replace("temp_", "")
+            if not os.path.exists(
+                os.path.join("static/uploads", filename)
+            ):
+                os.rename(
+                    os.path.join("static/uploads", temp_filename),
+                    os.path.join("static/uploads", filename)
+                )
+            else:
+                os.remove(
+                    os.path.join("static/uploads", temp_filename)
+                )
+            LAST_FILENAME = ""
+            return f"<h1>Файл {filename} успешно загружен.</h1><br><a href='/load_photo'>Загрузить ещё</a>"
+
+        file = request.files["file"]
+        filename = file.filename
+
+        if file:
+            if "file" in request.files:
+                if LAST_FILENAME != "":
+                    os.remove(
+                        os.path.join("static/uploads", LAST_FILENAME)
+                    )
+                filename = "temp_" + filename
+                temp_path = os.path.join("static/uploads", filename)
+                file.save(temp_path)
+                LAST_FILENAME = filename
+
+    return render_template('upload.html', message="", filename=filename)
 
 
 app.run(port=8888, host="127.0.0.1")
